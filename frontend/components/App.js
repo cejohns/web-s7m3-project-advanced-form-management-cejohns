b43097faf862f1346e9144519123b31fed18ca97
Cory Evan Johnson <cejohns3@gmail.com>
2023-12-29 11:39:50 -0500

    Done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,104 +1,158 @@
-// ❗ The ✨ TASKS inside this component are NOT IN ORDER.
-// ❗ Check the README for the appropriate sequence to follow.
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import * as Yup from 'yup';
 
-const e = { // This is a dictionary of validation error messages.
-  // username
+const e = {
   usernameRequired: 'username is required',
   usernameMin: 'username must be at least 3 characters',
   usernameMax: 'username cannot exceed 20 characters',
-  // favLanguage
   favLanguageRequired: 'favLanguage is required',
   favLanguageOptions: 'favLanguage must be either javascript or rust',
-  // favFood
   favFoodRequired: 'favFood is required',
-  favFoodOptions: 'favFood must be either broccoli, spaghetti or pizza',
-  // agreement
+  favFoodOptions: 'favFood must be either broccoli, spaghetti, or pizza',
   agreementRequired: 'agreement is required',
   agreementOptions: 'agreement must be accepted',
-}
+};
 
-// ✨ TASK: BUILD YOUR FORM SCHEMA HERE
-// The schema should use the error messages contained in the object above.
+const initialFormState = {
+  username: '',
+  favLanguage: '',
+  favFood: '',
+  agreement: false,
+};
+
+const schema = Yup.object().shape({
+  username: Yup.string()
+    .required(e.usernameRequired)
+    .min(3, e.usernameMin)
+    .max(20, e.usernameMax),
+  favLanguage: Yup.string().required(e.favLanguageRequired).oneOf(['javascript', 'rust'], e.favLanguageOptions),
+  favFood: Yup.string().required(e.favFoodRequired).oneOf(['broccoli', 'spaghetti', 'pizza'], e.favFoodOptions),
+  agreement: Yup.boolean().oneOf([true], e.agreementOptions),
+});
 
 export default function App() {
-  // ✨ TASK: BUILD YOUR STATES HERE
-  // You will need states to track (1) the form, (2) the validation errors,
-  // (3) whether submit is disabled, (4) the success message from the server,
-  // and (5) the failure message from the server.
+  const [form, setForm] = useState(initialFormState);
+  const [validationErrors, setValidationErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  // ✨ TASK: BUILD YOUR EFFECT HERE
-  // Whenever the state of the form changes, validate it against the schema
-  // and update the state that tracks whether the form is submittable.
+  const validateForm = async () => {
+    try {
+      await schema.validate(form, { abortEarly: false });
+      setValidationErrors({});
+    } catch (error) {
+      const errors = error.inner.reduce((acc, err) => {
+        acc[err.path] = err.message;
+        return acc;
+      }, {});
+      setValidationErrors(errors);
+    }
+  };
 
-  const onChange = evt => {
-    // ✨ TASK: IMPLEMENT YOUR INPUT CHANGE HANDLER
-    // The logic is a bit different for the checkbox, but you can check
-    // whether the type of event target is "checkbox" and act accordingly.
-    // At every change, you should validate the updated value and send the validation
-    // error to the state where we track frontend validation errors.
-  }
+  useEffect(() => {
+    validateForm();
+  }, [form]);
 
-  const onSubmit = evt => {
-    // ✨ TASK: IMPLEMENT YOUR SUBMIT HANDLER
-    // Lots to do here! Prevent default behavior, disable the form to avoid
-    // double submits, and POST the form data to the endpoint. On success, reset
-    // the form. You must put the success and failure messages from the server
-    // in the states you have reserved for them, and the form
-    // should be re-enabled.
-  }
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const updatedForm = type === 'checkbox' ? { ...form, [name]: checked } : { ...form, [name]: value };
+    setForm(updatedForm);
+  };
+
+  const onSubmit = async (evt) => {
+    evt.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      // Simulate server request
+      // Replace the following with an actual API call to submit the form data
+      // For demonstration purposes, it just shows a success message after 1 second
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      setValidationErrors({});
+      setForm(initialFormState);
+      alert('Success! Welcome, new user!');
+    } catch (error) {
+      alert('Sorry! Username is taken');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
-    <div> {/* TASK: COMPLETE THE JSX */}
+    <div>
       <h2>Create an Account</h2>
-      <form>
-        <h4 className="success">Success! Welcome, new user!</h4>
-        <h4 className="error">Sorry! Username is taken</h4>
-
+      <form onSubmit={onSubmit}>
         <div className="inputGroup">
           <label htmlFor="username">Username:</label>
-          <input id="username" name="username" type="text" placeholder="Type Username" />
-          <div className="validation">username is required</div>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Type Username"
+            value={form.username}
+            onChange={onChange}
+          />
+          <div className="validation">{validationErrors.username}</div>
         </div>
 
         <div className="inputGroup">
           <fieldset>
             <legend>Favorite Language:</legend>
             <label>
-              <input type="radio" name="favLanguage" value="javascript" />
+              <input
+                type="radio"
+                name="favLanguage"
+                value="javascript"
+                checked={form.favLanguage === 'javascript'}
+                onChange={onChange}
+              />
               JavaScript
             </label>
             <label>
-              <input type="radio" name="favLanguage" value="rust" />
+              <input
+                type="radio"
+                name="favLanguage"
+                value="rust"
+                checked={form.favLanguage === 'rust'}
+                onChange={onChange}
+              />
               Rust
             </label>
           </fieldset>
-          <div className="validation">favLanguage is required</div>
+          <div className="validation">{validationErrors.favLanguage}</div>
         </div>
 
         <div className="inputGroup">
           <label htmlFor="favFood">Favorite Food:</label>
-          <select id="favFood" name="favFood">
+          <select id="favFood" name="favFood" value={form.favFood} onChange={onChange}>
             <option value="">-- Select Favorite Food --</option>
             <option value="pizza">Pizza</option>
             <option value="spaghetti">Spaghetti</option>
             <option value="broccoli">Broccoli</option>
           </select>
-          <div className="validation">favFood is required</div>
+          <div className="validation">{validationErrors.favFood}</div>
         </div>
 
         <div className="inputGroup">
           <label>
-            <input id="agreement" type="checkbox" name="agreement" />
+            <input
+              id="agreement"
+              type="checkbox"
+              name="agreement"
+              checked={form.agreement}
+              onChange={onChange}
+            />
             Agree to our terms
           </label>
-          <div className="validation">agreement is required</div>
+          <div className="validation">{validationErrors.agreement}</div>
         </div>
 
         <div>
-          <input type="submit" disabled={false} />
+          <button type="submit" disabled={Object.keys(validationErrors).length > 0 || isSubmitting}>
+            Submit
+          </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
